75b78bde058a86ec57644ea56cdd4fb56fea3acc
Darrell Walker <walkerdarrell15@gmail.com>
2026-05-28 21:47:56 -0400

    added delete api

--- router/auth_users.js
+++ router/auth_users.js
@@ -54,6 +54,23 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   }
 });
 
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+  
+    if (books[isbn]) {
+      if (books[isbn].reviews[username]) {
+        delete books[isbn].reviews[username];
+        return res.status(200).json({ message: `Reviews for the ISBN ${isbn} posted by the user ${username} deleted.` });
+      } else {
+        return res.status(404).json({ message: "No review by this user for this book" });
+      }
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  });
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
